bfd0ffbe0fe342d0c0f365f58bfb3a49945e65da
CeliFioretti <celi.fioretti.12@gmail.com>
2025-07-27 18:47:38 -0300

    Arreglo back CORS

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,11 @@ const rutasEventosOperador = require('./routes/eventos-operador.routes.js');
 const rutaExport = require('./routes/export.routes.js');
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+   origin: [
+    'https://logirefrigeracion-1-3vug.onrender.com', 
+    'http://localhost:5173',
+  ],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
